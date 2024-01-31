e6d1cab9d59086a470a0ee7c6a463ba3eb5feaea
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2024-01-31 13:57:03 +0000

    Attempt to fix build error

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,41 +1,34 @@
-import Header from "@/app/components/common/Header";
+import Header from '@/app/components/common/Header'
 
 export default function Home() {
-    return (
-        <>
-            <Header />
-            <main className="relative h-screen-sm pt-12 flex flex-col">
-                <section className="p-12">
-                    <h1 className="text-4xl font-bold">
-                        BRAND
-                        <br />
-                        HUNT
-                        <br />
-                        <span className="font-extralight">2024</span>
-                    </h1>
-                </section>
-                <section className="relative pt-16 px-12 flex-1 overflow-hidden">
-                    <div className="absolute right-1/2 top-0 translate-x-1/2 w-[900px] h-[900px] bg-gray-300 rounded-full" />
-                    <div className="relative">
-                        <h2 className="text-2xl font-medium pb-4">
-                            Your goals are:
-                        </h2>
-                        <ol className="list-decimal pl-6 mb-8 font-light">
-                            <li>Scan QR codes to gain jigsaw pieces.</li>
-                            <li>
-                                Put them together to solve the final puzzle.
-                            </li>
-                            <li>
-                                Share your solution with friends on social
-                                media.
-                            </li>
-                        </ol>
-                        <p className="text-center font-medium border border-black p-2 rounded-lg">
-                            GET STARTED.
-                        </p>
-                    </div>
-                </section>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main className='relative h-screen-sm pt-12 flex flex-col'>
+        <section className='p-12'>
+          <h1 className='text-4xl font-bold'>
+            BRAND
+            <br />
+            HUNT
+            <br />
+            <span className='font-extralight'>2024</span>
+          </h1>
+        </section>
+        <section className='relative pt-16 px-12 flex-1 overflow-hidden'>
+          <div className='absolute right-1/2 top-0 translate-x-1/2 w-[900px] h-[900px] bg-gray-300 rounded-full' />
+          <div className='relative'>
+            <h2 className='text-2xl font-medium pb-4'>Your goals are:</h2>
+            <ol className='list-decimal pl-6 mb-8 font-light'>
+              <li>Scan QR codes to gain jigsaw pieces.</li>
+              <li>Put them together to solve the final puzzle.</li>
+              <li>Share your solution with friends on social media.</li>
+            </ol>
+            <p className='text-center font-medium border border-black p-2 rounded-lg'>
+              GET STARTED.
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
--- app/components/puzzle/Jigsaw.jsx
+++ app/components/puzzle/Jigsaw.jsx
@@ -1,120 +1,121 @@
-import { useState, useEffect } from "react";
-import { arraysAreEqual } from "@/app/utils/helpers";
+import { useState, useEffect } from 'react'
+import { arraysAreEqual } from '@/app/utils/helpers'
 
 export default function Jigsaw() {
-    const pieces = [
-        {
-            id: 0,
-            content: "D",
-            solutionIndex: 3,
-        },
-        {
-            id: 1,
-            content: "B",
-            solutionIndex: 1,
-        },
-        {
-            id: 2,
-            content: "F",
-            solutionIndex: 5,
-        },
-        {
-            id: 3,
-            content: "C",
-            solutionIndex: 2,
-        },
-        {
-            id: 4,
-            content: "A",
-            solutionIndex: 0,
-        },
-        {
-            id: 5,
-            content: "E",
-            solutionIndex: 4,
-        },
-    ];
+  const pieces = [
+    {
+      id: 0,
+      content: 'D',
+      solutionIndex: 3,
+    },
+    {
+      id: 1,
+      content: 'B',
+      solutionIndex: 1,
+    },
+    {
+      id: 2,
+      content: 'F',
+      solutionIndex: 5,
+    },
+    {
+      id: 3,
+      content: 'C',
+      solutionIndex: 2,
+    },
+    {
+      id: 4,
+      content: 'A',
+      solutionIndex: 0,
+    },
+    {
+      id: 5,
+      content: 'E',
+      solutionIndex: 4,
+    },
+  ]
+
+  const [boardPieces, setBoardPieces] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ])
+  const [selectedPieceId, setSelectedPieceId] = useState(null)
+
+  useEffect(() => {
     const orderedPieces = [...pieces].sort(
-        (a, b) => a.solutionIndex - b.solutionIndex
-    );
-    let solutionPieces = [];
-    orderedPieces.forEach((piece) => solutionPieces.push(piece.id));
+      (a, b) => a.solutionIndex - b.solutionIndex
+    )
+    let solutionPieces = []
+    orderedPieces.forEach((piece) => solutionPieces.push(piece.id))
 
-    // const [boardPieces, setBoardPieces] = useState([null, 1, 3, 0, 5, 2]);
-    const [boardPieces, setBoardPieces] = useState([
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-    ]);
-    const [selectedPieceId, setSelectedPieceId] = useState(null);
+    if (arraysAreEqual(solutionPieces, boardPieces)) {
+      console.log('puzzle complete!')
+    }
+  }, [boardPieces])
 
-    useEffect(() => {
-        console.log("board changed");
-        if (arraysAreEqual(solutionPieces, boardPieces)) {
-            console.log("puzzle complete!");
-        }
-    }, [boardPieces]);
+  useEffect(
+    () => console.log(`selectedPieceId: ${selectedPieceId}`),
+    [selectedPieceId]
+  )
 
-    return (
-        <div className="flex flex-col h-full">
-            <div className="flex-1 px-12">
-                <ul className="grid grid-cols-2 gap-1 w-full h-full">
-                    {boardPieces.map((boardPiece, i) =>
-                        boardPiece !== null ? (
-                            <li
-                                className="w-full h-full border border-black border-dashed"
-                                style={{
-                                    borderColor: selectedPieceId
-                                        ? "orange"
-                                        : "black",
-                                }}
-                                onClick={(e) => {
-                                    let newBoardPieces = [...boardPieces];
-                                    newBoardPieces[i] = selectedPieceId;
-                                    setBoardPieces([...newBoardPieces]);
-                                    setSelectedPieceId(null);
-                                }}
-                            >
-                                {pieces[boardPiece].content}
-                            </li>
-                        ) : (
-                            <li
-                                className="w-full h-full border border-black border-dashed"
-                                style={{
-                                    borderColor: selectedPieceId
-                                        ? "orange"
-                                        : "black",
-                                }}
-                                onClick={(e) => {
-                                    let newBoardPieces = [...boardPieces];
-                                    newBoardPieces[i] = selectedPieceId;
-                                    setBoardPieces([...newBoardPieces]);
-                                    setSelectedPieceId(null);
-                                }}
-                            ></li>
-                        )
-                    )}
-                </ul>
-            </div>
-            <div className="h-60 px-12">
-                <ul className="grid grid-cols-3 gap-1 w-full h-full">
-                    {pieces
-                        .filter((piece) => !boardPieces.includes(piece.id))
-                        .map((piece, i) => (
-                            <li
-                                className="w-full h-full border border-black bg-gray-400 flex justify-center items-center text-center"
-                                onClick={() => {
-                                    setSelectedPieceId(piece.id);
-                                }}
-                            >
-                                {piece.content}
-                            </li>
-                        ))}
-                </ul>
-            </div>
-        </div>
-    );
+  const reorderPieces = (i) => {
+    if (selectedPieceId !== null) {
+      let newBoardPieces = [...boardPieces]
+      newBoardPieces[i] = selectedPieceId
+      setBoardPieces([...newBoardPieces])
+      setSelectedPieceId(null)
+    } else {
+      console.log('piece already selected')
+    }
+  }
+
+  return (
+    <div className='flex flex-col h-full'>
+      <div className='flex-1 px-12'>
+        <ul className='grid grid-cols-2 gap-1 w-full h-full'>
+          {boardPieces.map((boardPiece, i) =>
+            boardPiece !== null ? (
+              <li
+                className='w-full h-full border border-black border-dashed'
+                style={{
+                  borderColor: selectedPieceId ? 'orange' : 'black',
+                }}
+                onClick={() => reorderPieces(i)}
+              >
+                {pieces[boardPiece].content}
+              </li>
+            ) : (
+              <li
+                className='w-full h-full border border-black border-dashed'
+                style={{
+                  borderColor: selectedPieceId ? 'orange' : 'black',
+                }}
+                onClick={() => reorderPieces(i)}
+              ></li>
+            )
+          )}
+        </ul>
+      </div>
+      <div className='h-60 px-12'>
+        <ul className='grid grid-cols-3 gap-1 w-full h-full'>
+          {pieces
+            .filter((piece) => !boardPieces.includes(piece.id))
+            .map((piece, i) => (
+              <li
+                className='w-full h-full border border-black bg-gray-400 flex justify-center items-center text-center'
+                onClick={() => {
+                  setSelectedPieceId(piece.id)
+                }}
+              >
+                {piece.content}
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
