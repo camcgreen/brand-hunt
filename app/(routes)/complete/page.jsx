9770384d232c8aa907bdc8b46ac0bc1091d198de
camcgreen <c.c.green@outlook.com>
2024-03-25 14:12:58 +0000

    Remove social media share icons and replace with image download button

--- app/(routes)/complete/page.jsx
+++ app/(routes)/complete/page.jsx
@@ -24,18 +24,29 @@ export default function Complete() {
         </section>
         <section className='relative pt-16 px-12 flex-1 overflow-hidden'>
           <div className='absolute right-1/2 top-0 translate-x-1/2 w-[900px] h-[900px] bg-gray-300 rounded-full' />
-          <div className='relative'>
+          <div className='relative flex flex-col items-center'>
             <h2 className='text-2xl font-medium pb-4 text-center'>
-              We hope you enjoyed playing!
+              #YourHashtag
             </h2>
-            <p className='text-center mb-12'>
-              Don&apos;t forget to share your success.
+            <p className='text-center mb-12 font-light'>
+              Download your completed image here to share on social media.
             </p>
-            <div className='h-8 flex justify-around'>
+            <a
+              className='bg-black rounded-full p-4'
+              href='/images/complete.jpg'
+              download
+            >
+              <img
+                src='/images/download.png'
+                alt='Download image'
+                className='w-4'
+              />
+            </a>
+            {/* <div className='h-8 flex justify-around'>
               <img src='/images/fb.svg' alt='Share on Facebook' />
               <img src='/images/ig.svg' alt='Share on Instagram' />
               <img src='/images/x.svg' alt='Share on X' />
-            </div>
+            </div> */}
           </div>
         </section>
       </main>
